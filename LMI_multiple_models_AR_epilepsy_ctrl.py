5eb70109b5a463a5193d6e78f8f599a86986c51c
lheureduloup <61148296+lheureduloup@users.noreply.github.com>
2023-08-21 20:33:53 +0200

    Update LMI_multiple_models_AR_epilepsy_ctrl.py

--- LMI_multiple_models_AR_epilepsy_ctrl.py
+++ LMI_multiple_models_AR_epilepsy_ctrl.py
@@ -12,24 +12,18 @@ from multiple_models_AR_epilepsy import A_cont
 A_base = A_cont
 n_models = len(A_cont)
 Nm = len(A_cont[0])
-B = np.identity(Nm)
+B = np.zeros((Nm,Nm))
 C = np.zeros((1,Nm))
 
-alpha = 20
-
-#SUB: 32 -- alpha = 40, b = 8.5e-5 (PIS) // alpha = 20, b = 8.5e-05 (input), but general is 8e-5
-#SUB: 21 -- alpha = 20  b = 7e-5  (II)   // alpha = 0,  b = 5.0e-05 (input), but general is 7e-5
-#DG:  27 -- alpha = 0, b = 9e-6 (PIS) 
-#CA2: 29 -- alpha = 10, b = 8e-10  (PIS)
-#CA1: 30 -- alpha = 20, b = 1e-4  (PIS)
+alpha = 1680 # 1680/2 non-hybrid (SUB), 810/1.2 hybrid (SUB)
 
 cte = 0
-B[0][0] = 8e-5
-B[1][1] = cte
-B[2][2] = cte
-B[3][3] = cte
-B[4][4] = cte
-B[5][5] = cte
+B[0][0] = 1
+B[1][1] = 1
+B[2][2] = 1
+B[3][3] = 1
+B[4][4] = 1
+B[5][5] = 1
 
 C[0][0] = 1
 
@@ -42,6 +36,7 @@ BB = pic.new_param('B', cvx.matrix(B))
 CC = pic.new_param('C', cvx.matrix(C))
 Gx = prob.add_variable('Gx', (Nm,Nm), vtype='symmetric')
 X = prob.add_variable('X', (Nm,Nm), vtype='symmetric')
+Q = prob.add_variable('Q', (Nm,Nm), vtype='symmetric')
 
 for op in range(0,n_models):  
     Am = A_base[op]
